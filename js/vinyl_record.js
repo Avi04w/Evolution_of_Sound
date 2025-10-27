20f841b16aa18da5645cbb6b88c659740d4e1c0c
ethanliu24 <liu.ethan2005@gmail.com>
2025-10-27 19:38:02 -0400

    add visual indicator for pause on player

--- js/vinyl_record.js
+++ js/vinyl_record.js
@@ -3,8 +3,11 @@ class VinylRecord {
     this.outerShell = document.getElementById("record-player-shell-out");
     this.imageEl = document.getElementById("record-player-image");
     this.audioEl = document.getElementById("player");
+    this.pauseEl = document.getElementById("record-player-pause");
     this.playing = false;
 
+    this.outerShell.addEventListener("mouseenter", () => this.showPausePanel());
+    this.outerShell.addEventListener("mouseleave", () => this.hidePausePanel());
     this.outerShell.addEventListener("click", () => this.playing ? this.pause() : this.play());
     this.audioEl.addEventListener("ended", () => {
       this.audioEl.currentTime = 0;
@@ -14,6 +17,7 @@ class VinylRecord {
 
   async load(songUrl, imageUrl) {
     this.outerShell.style.animationPlayState = "paused";
+    this.pauseEl.style.animationPlayState = "paused";
     this.audioEl.src = songUrl;
     this.imageEl.style.removeProperty("display");
     this.imageEl.src = imageUrl;
@@ -24,16 +28,28 @@ class VinylRecord {
   play() {
     this.playing = true;
     this.audioEl.play().catch(err => console.error("Audio error:", err));
-    this.outerShell.style.animationPlayState = "running"
+    this.outerShell.style.animationPlayState = "running";
+    this.pauseEl.style.animationPlayState = "running";
+    this.pauseEl.querySelector("span").textContent = "❚❚";
   }
 
   pause() {
     this.playing = false;
     this.outerShell.style.animationPlayState = "paused";
+    this.pauseEl.style.animationPlayState = "paused";
     this.audioEl.pause();
+    this.pauseEl.querySelector("span").textContent = "▶";
   }
 
   isPaused() {
     return this.audioEl.paused;
+  }
+
+  showPausePanel() {
+    if (this.audioEl.src) this.pauseEl.style.opacity = 1;
+  }
+
+  hidePausePanel() {
+    this.pauseEl.style.opacity = 0;
   }
 }
